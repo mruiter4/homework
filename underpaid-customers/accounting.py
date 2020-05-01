d3992d75be6bee2503fb0d2982512e3116884d00
Michelle Ruiter <mruiter4@gmail.com>
2020-04-30 20:59:01 -0700

    Created report layout

--- underpaid-customers/accounting.py
+++ underpaid-customers/accounting.py
@@ -2,22 +2,38 @@ def find_underpaid_customer_accounts(file):
   """ finds customers that owe payment"""
 
   melon_cost = 1.00
-
+  #open files
   file = open(file)
+
   for line in file:
+    #read line in file and parse input
     line = line.rstrip()
     words = line.split('|')
 
-    name = words[1]
+    fullname = words[1]
+    name_parts = fullname.split()
+    first_name = name_parts[0]
+    last_name = name_parts[1]
+
     num_of_melons = float(words[2])
+    #print(num_of_melons)
     amount_paid = float(words[3])
-    amount_paid = format(amount_paid, '.2f')
 
+    #calculate expected payment
     expected_payment = num_of_melons * melon_cost
-    expected_payment = format(expected_payment, '.2f')
 
-    if amount_paid != expected_payment:
-      print(f'{name} paid ${amount_paid}, expected ${expected_payment}')
+
+    #print all customer data
+    print(f'{fullname} paid ${amount_paid:.2f}, expected ${expected_payment:.2f}')
+
+    #print under and over paid accts
+    if amount_paid > expected_payment:
+      print(f'{first_name} overpaid for their melons')
+    elif amount_paid < expected_payment:
+      print(f'{first_name} underpaid for their melons')
+
+  #close file
+  file.close()
     
 
 find_underpaid_customer_accounts("customer-orders.txt")
