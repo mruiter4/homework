647cb80204324860bb3ca84aec32154b5368d9ac
Michelle Ruiter <mruiter4@gmail.com>
2020-05-06 19:19:31 -0700

    Completed run_raffle and pick_winner

--- melon-raffle/version2/raffle.py
+++ melon-raffle/version2/raffle.py
@@ -1,6 +1,20 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from customers import get_customers_from_file
+from random import choice
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+def pick_winner(customers):
+    """pick a random winner from customer list"""
+
+    winner = choice(customers)
+    print(f'Tell {winner.name} at {winner.email} they are the winner!')
+
+
+def run_raffle(filename):
+    """run the raffle"""
+
+    customers = get_customers_from_file(filename)
+    pick_winner(customers)
+
+if __name__ == '__main__':
+    run_raffle('customers.txt')
